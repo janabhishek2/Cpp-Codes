526f157078d9c0801aa569a42b46bdd779689536
Abhishek Jan <abhishek.jan@indiamart.com>
2022-01-14 22:38:09 +0530

    test

--- adv-cpp/printAllSubArrays.cpp
+++ adv-cpp/printAllSubArrays.cpp
@@ -4,9 +4,32 @@ void printAllSubArrays(int arr[],int n){
 
     for(int i=0;i<n;i++){
         for(int j=i;j<n;j++){
-            cout<<arr[i]
+
+            for(int k=i;k<=j;k++){
+                cout<<arr[k]<<" ";
+            }
+            cout<<endl;
         }
+
     }
+}
+int maxSumOfSubArray(int arr[],int n){
+    int maxSum=-1;
+
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+                int sum=0;
+            for(int k=i;k<=j;k++){
+                sum+=arr[k];
+            }
+            if(sum>maxSum){
+                maxSum=sum;
+            }
+        }
+
+    }
+    return maxSum;
+
 }
 int main(){
 
@@ -18,5 +41,6 @@ cout<<"\nEnter Elements : ";
 for(int i=0;i<n;i++){
     cin>>arr[i];
 }
-printAllSubArrays(arr,n);
+int mxSum=maxSumOfSubArray(arr,n);
+cout<<mxSum;
 }
